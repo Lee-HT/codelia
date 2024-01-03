c0419abc3c8d8940bfb0d9acfa54770e7b66f35c
LEE-HT <12gusxkr@naver.com>
2024-01-03 16:13:25 +0900

    path 수정이 용이하도록 route 수정, 불필요한 기본 파일 삭제

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,32 +1,36 @@
 import { LoginContext } from "contexts/Login/LoginContext";
 import { useContext, useEffect } from "react";
 import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const { userInfo } = useContext(LoginContext);
-
-  useEffect(() => {});
+  const navigate = useNavigate();
 
   const providers = ["google", "naver"];
 
   return (
     <div className="login-container">
-      <div className="login-area">
+      {userInfo.isLogin ? (
+        navigate("/")
+      ) : (
+        <div className="login-area">
           {providers?.map((provider) => {
             return (
-                <a
-                  title={provider + " 로그인"}
-                  href={
-                    process.env.REACT_APP_HOST +
-                    "/api/oauth2/authorization/" +
-                    provider
-                  }
-                >
-                  {provider} 로그인
-                </a>
+              <a
+                title={provider + " 로그인"}
+                href={
+                  process.env.REACT_APP_HOST +
+                  "/api/oauth2/authorization/" +
+                  provider
+                }
+              >
+                {provider} 로그인
+              </a>
             );
           })}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
